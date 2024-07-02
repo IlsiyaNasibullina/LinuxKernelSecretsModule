e6b5f9560469e944cd230ebf33ea88fc26e123ef
Ilsiia <i.nasibullina@innopolis.university>
2024-07-02 18:36:59 +0300

    Определена структура struct secret для хранения секретов. Имплементирована функция secrets_write для записи секрета в ядро.

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ MODULE_DESCRIPTION("Secrets driver");
 MODULE_LICENSE("GPL");
 
 #define PROC_NAME "secrets" // The name of the proc file
+#define BUFSIZE 300 // The buffer size
 
 static struct proc_dir_entry *proc_entry; // Pointer to proc directory entry
 
@@ -22,6 +23,15 @@ static struct proc_ops proc_file_ops = {
         .proc_delete = secrets_delete
 };
 
+// Structure of secret with id and secret data
+struct secret {
+    int id;
+    char data[BUFSIZE];
+};
+
+static LIST_HEAD(secret_list); // Lined list head for keeping track of stored secrets
+static int next_id = 1;
+
 // Read secret function
 static int secrets_read(){
     return 0;
@@ -29,9 +39,43 @@ static int secrets_read(){
 
 
 // Write secret function
-static int secrets_write(){
-    return 0;
+static ssize_t secrets_write(struct file *file, const char __user *buf, size_t count, loff_t *pos){
+    struct secret *new_secret;
+    char tmp[BUFSIZE];
+    int len;
+
+    // Return error if the data size exceeds BUFSIZE
+    if (count >= BUFSIZE) {
+        return -EINVAL;
+    }
+
+    // Allocation of memory for new_secret structure
+    new_secret = kmalloc(sizeof(*new_secret), GFP_KERNEL);
+    if (!new_secret) {
+        return -ENOMEM; // Return out of memory error if allocation fails
+    }
+
+    // Copy user secret from buf to tmp buffer
+    if (copy_from_user(tmp, buf, count)) {
+        kfree(new_secret);
+        return -EFAULT;
+    }
+    tmp[count] = '\0';
+
+    // Assign id to new secret and copy data into struct
+    new_secret->id = next_id++;
+    strncpy(new_secret->data, tmp, BUFSIZE);
+    list_add(&new_secret->list, &secret_list);
+
+    // Response to user space with id of new secret
+    len = snprintf(tmp, BUFSIZE, "%d\n", new_secret->id);
+    if (copy_to_user(buf, tmp, len)) {
+        return -EFAULT;
+    }
+
+    return len;
 }
+
 
 // Delete secret function
 static int secrets_delete(){
